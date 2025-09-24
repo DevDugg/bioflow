fda49a85c777e39e24c5d7970e5ae2ad217faf46
DevDugg <mr.aydogdy02@gmail.com>
2025-09-24 15:56:37 +0500

    added Supabase environment variables for local testing

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -1,3 +1,6 @@
 import "@testing-library/jest-dom";
 
 process.env.NEXT_PUBLIC_SITE_URL = "http://localhost:3000";
+process.env.NEXT_PUBLIC_SUPABASE_URL = "http://localhost:54321";
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = "test";
+process.env.SUPABASE_SERVICE_ROLE_KEY = "test";
